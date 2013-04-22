829223c64e9fa1ae5562d99590e1ff1d4d1c314c
balasubrahmanya balakrishna soe student <bb245@afsconnect1.njit.edu>
2013-04-22 13:31:17 -0400

    IP converstion

---
+++ Datavizualization/inet.js
@@ -0,0 +1,13 @@
+var ipint = require('ipint');
+
+function aton(incoming_ip) {
+
+	return ipint.ipToInt(incoming_ip);
+}
+
+function ntoa(incoming_int_ip){
+
+	return ipint.intToIp(incoming_int_ip);
+}
+exports.aton = aton;
+exports.ntoa = ntoa;
